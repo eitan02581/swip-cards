ed1485c059e836909c1a909aa6b53b04853f7a02
Eitan Elnekave <36443745+eitan02581@users.noreply.github.com>
2020-08-24 12:44:15 +0300

    1

---
+++ precache-manifest.d0beaacb05e2935916ee844df1fa36d3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "308d6674ae7d69093bcd",
+    "url": "/css/app.ba32e4a9.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "66fbed4a69bbb4654d2487cff062160e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7ccb07e9795d55fdaab",
+    "url": "/js/about.c795331c.js"
+  },
+  {
+    "revision": "308d6674ae7d69093bcd",
+    "url": "/js/app.85ac9e55.js"
+  },
+  {
+    "revision": "c3923c061b1665899eb0",
+    "url": "/js/chunk-vendors.390b6492.js"
+  },
+  {
+    "revision": "275832b095c4938c63a8201b2d03eca8",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
